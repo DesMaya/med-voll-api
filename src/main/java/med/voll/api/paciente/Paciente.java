4e21c916159f5908cd0d08adb482a55cabff2e34
Maya <mayaflorentinodasilva@gmail.com>
2025-01-07 16:32:12 -0300

    Metodos para realizar o UPDATE e o DELETE lógico para Pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -15,6 +15,7 @@ public class Paciente {
     private String email;
     private String cpf;
     private String telefone;
+    private Boolean ativo;
 
     @Embedded
     private Endereco endereco;
@@ -25,10 +26,26 @@ public class Paciente {
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
     public Paciente() {
 
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+
+        if (dados.telefone() != null)
+            this.telefone = dados.telefone();
+
+        if (dados.endereco() != null)
+            endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void inativar() {
+        this.ativo = false;
     }
 
     public Long getId() {
