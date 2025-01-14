f5122ee54f3dd962944c8805d3f1dd93f4dd26ea
Maya <mayaflorentinodasilva@gmail.com>
2025-01-14 19:56:39 -0300

    Criados validadores para cada uma das regras de negocio utilizando o padrão SOLID

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var agora = LocalDate.now();
+        var diferencaEmMituos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if(diferencaEmMituos <= 30) {
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+        }
+    }
+}
